158f523ee8748339051f033734f4b642d4d846c0
hanzala211 <hanzalaoc211@gmail.com>
2024-11-08 12:13:32 +0500

    check

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: '_redirects',
-					dest: '.', // Copies _redirects to the root of dist folder
-				},
-			],
-		}),
-	],
+	plugins: [react()],
 });
