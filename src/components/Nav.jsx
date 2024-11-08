0ebb2b7c8397dd4fba90280c2b7dde2d28f5de7b
hanzala211 <hanzalaoc211@gmail.com>
2024-11-08 15:59:00 +0500

    origin

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -48,7 +48,7 @@ export function Nav() {
     }
 
     return <nav className={`flex justify-between items-center py-1 sm:py-0 sm:px-12 px-1.5 h-[50px] sm:h-[70px] transition duration-100 fixed ${isScrolled ? "bg-[#141414] z-[100]" : "bg-transparent"} w-[100vw] z-10`}>
-        <div className="flex items-start sm:items-center sm:gap-5 gap-2">
+        <div className="flex items-start sm:items-center sm:gap-5 gap-1">
             <Link to="/browse" onClick={() => setSearchQuery("")}><img src="/images/download1.png" alt="Netflix Log" className="sm:w-[7.5rem] w-[4rem]" /></Link>
             <div className="sm:hidden">
                 <select
@@ -102,8 +102,8 @@ export function Nav() {
         </div>
 
         <div className="flex sm:gap-5 gap-2 items-center bg-transparent">
-            <div className="relative flex justify-end sm:block">
-                <BsSearch onClick={toggleSearch} className={`sm:text-[17px] text-[13px] cursor-pointer transition-[left] duration-300 ease-out ${isSearching ? 'absolute top-1/2 -translate-y-1/2 left-[4rem] sm:left-2 ' : 'sm:translate-y-3 translate-y-1.5'}`} />
+            <div className="relative flex justify-end w-40 sm:w-auto sm:block">
+                <BsSearch onClick={toggleSearch} className={`sm:text-[17px] text-[13px] cursor-pointer transition-[left] duration-300 ease-out ${isSearching ? 'absolute top-1/2 -translate-y-1/2 375:left-[2.3rem] 400:left-[4rem] sm:left-2 ' : 'sm:translate-y-3 translate-y-1.5'}`} />
                 <input
                     type="text"
                     ref={searchRef}
@@ -111,7 +111,7 @@ export function Nav() {
                     onChange={(e) => {
                         setSearchQuery(e.target.value)
                     }}
-                    className={`bg-[#141414] border-[0px] overflow-hidden rounded-sm text-white focus-within:outline-none ${isSearching ? "sm:px-9 px-6 py-1 sm:w-full w-[70%] sm:text-[17px] text-[12px] border-[1px] transition-[padding-left] sm:transition-[padding-right,padding-left] duration-500" : "w-0 px-0 py-0 "}`}
+                    className={`bg-[#141414] border-[0px] overflow-hidden rounded-sm text-white focus-within:outline-none ${isSearching ? "sm:px-9 px-6 pr-2 py-1 sm:w-full w-[80%] sm:text-[17px] text-[12px] border-[1px] transition-[padding-left] sm:transition-[padding-right,padding-left] duration-500" : "w-0 px-0 py-0 "}`}
                     placeholder="Titles, people, genres"
                 />
             </div>
